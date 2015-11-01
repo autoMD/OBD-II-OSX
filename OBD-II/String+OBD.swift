4064ae9bfc9ef0bdcdc73e97ec39734beae18716
agrippa1994 <agrippa1994@icloud.com>
2015-11-01 21:24:40 +0100

    OBD command parsing

--- OBD-II/String+OBD.swift
+++ OBD-II/String+OBD.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+extension String {
+    var isValidOBD: Bool {
+        return self.toOBD != nil
+    }
+    
+    var toOBD: [UInt8]? {
+        if self.isEmpty {
+            return nil
+        }
+        
+        var bytes = [UInt8]()
+        let tokens = self.characters.split { $0 ==   " " }.map(String.init)
+        for token in tokens {
+            guard let byte = UInt8(token, radix: 16) else {
+                return nil
+            }
+            
+            bytes.append(byte)
+        }
+        
+        return bytes
+    }
+}
