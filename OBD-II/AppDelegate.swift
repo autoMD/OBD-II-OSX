32ec9337fbf8f7515751130e3df9ad538c88b61d
agrippa1994 <agrippa1994@icloud.com>
2015-11-01 20:23:13 +0100

    Initial commit

---
+++ OBD-II/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OBD-II
+//
+//  Created by Manuel Stampfl on 01.11.15.
+//  Copyright © 2015 mani1337. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OBD-II/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OBD-II
+//
+//  Created by Manuel Stampfl on 01.11.15.
+//  Copyright © 2015 mani1337. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
